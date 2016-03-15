5d28b67f6c8269bd0606f68c6ebea0236a849b4f
Kit La Touche <kit@transneptune.net>
2016-03-14 22:14:00 -0600

    Fix up #41.
    Switched to X-Editable from Jeditable.
    
    Made related backend changes. We really need to move towards a DRF-based
    backend API; the per-ajax, per-method hacking is terrible.
    
    Closes #27.

--- ex_libris/books/forms.py
+++ ex_libris/books/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.forms.fields import FileField
+
 from taggit.forms import TagField
 
 from .models import (
@@ -8,6 +11,44 @@ from .models import (
     Book,
     BookProfile,
 )
+
+
+# From https://djangosnippets.org/snippets/2758/
+class PatchModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        for fieldname in self.base_fields:
+            self.base_fields[fieldname].required = False
+        self.empty_permited = True
+        return super().__init__(*args, **kwargs)
+
+    def _clean_fields(self):
+        for name, field in self.fields.items():
+            # value_from_datadict() gets the data from the data dictionaries.
+            # Each widget type knows how to retrieve its own data, because some
+            # widgets split data over several HTML fields.
+            value = field.widget.value_from_datadict(
+                self.data,
+                self.files,
+                self.add_prefix(name)
+            )
+            # this is the key difference with the inherited behavior so fields
+            # not present in the bound data do not get updated with None values
+            if self.add_prefix(name) not in self.data:
+                continue
+            try:
+                if isinstance(field, FileField):
+                    initial = self.initial.get(name, field.initial)
+                    value = field.clean(value, initial)
+                else:
+                    value = field.clean(value)
+                self.cleaned_data[name] = value
+                if hasattr(self, 'clean_{}'.format(name)):
+                    value = getattr(self, 'clean_{}'.format(name))()
+                    self.cleaned_data[name] = value
+            except ValidationError as e:
+                self._errors[name] = self.error_class(e.messages)
+                if name in self.cleaned_data:
+                    del self.cleaned_data[name]
 
 
 class AuthorForm(forms.ModelForm):
@@ -34,7 +75,7 @@ class SeriesForm(forms.ModelForm):
         )
 
 
-class BookForm(forms.ModelForm):
+class BookForm(PatchModelForm):
     author_name = forms.CharField(required=False)
     publisher_name = forms.CharField(required=False)
     series_name = forms.CharField(required=False)
@@ -60,10 +101,12 @@ class BookForm(forms.ModelForm):
             (Series, 'series'),
         )
         for model, field in model_field_pairs:
-            instance, _ = model.objects.get_or_create(
-                name=self.cleaned_data[field + "_name"],
-            )
-            setattr(obj, field, instance)
+            name = self.cleaned_data.get(field + '_name')
+            if name is not None:
+                instance, _ = model.objects.get_or_create(
+                    name=self.cleaned_data[field + "_name"],
+                )
+                setattr(obj, field, instance)
         obj.save()
         return obj
 
