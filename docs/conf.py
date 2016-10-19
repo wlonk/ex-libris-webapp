5ede57a88b70e52852239913db83efc8011e7bf0
Kit La Touche <kit@transneptune.net>
2016-10-19 09:20:14 -0600

    Include more Sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,9 @@ import sys  # NOQA
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
     'sphinxcontrib.httpdomain',
 ]
 
