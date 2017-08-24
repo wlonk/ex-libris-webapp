f3607620911c267e42eee70cdd2285cd08cfc71c
Kit La Touche <kit@transneptune.net>
2017-08-23 21:04:24 -0600

    Remove extraneous providers app

--- src/ex_libris/providers/custom_dropbox/urls.py
+++
@@ -1,5 +0,0 @@
-from allauth.socialaccount.providers.oauth.urls import default_urlpatterns
-
-from .provider import CustomDropboxOAuth2Provider
-
-urlpatterns = default_urlpatterns(CustomDropboxOAuth2Provider)
